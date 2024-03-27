be4374ddc0bc51d35ce9826e398904fc1a692b13
unknown <jatvivek658@gmail.com>
2024-03-27 14:44:49 +0530

    commiting changes

--- cypress/e2e/example/runner.js
+++ cypress/e2e/example/runner.js
@@ -9,42 +9,42 @@ import TestCase08 from "../../integration/testCases/TestCase08";
 import TestCase09 from "../../integration/testCases/TestCase09";
 import TestCase10 from "../../integration/testCases/TestCase10";
 describe('Test Suite', () => {
-    // it('First Test Case', () => {
-    //     const firstTestCase = new TestCase01();
-    //     firstTestCase.TestCase01();
-    // });
-    // it('Test Case No.2', () => {
-    //     const testCase02 = new TestCase02();
-    //     testCase02.testCase02();
-    // });
-    // it('Test Case No.3', () => {
-    //     const testCase03 = new TestCase03();
-    //     testCase03.testCase03();
-    // });
-    // it('Test Case No.4', () => {
-    //     const testCase04 = new TestCase04();
-    //     testCase04.testCase04();
-    // });
-    // it('Test Case No.5', () => {
-    //     const testCase05 = new TestCase05();
-    //     testCase05.testCase05();
-    // });
-    // it('Test Case No.6', () => {
-    //     const testCase06 = new TestCase06();
-    //     testCase06.testCase06();
-    // });
-    // it('Test Case No.7', () => {
-    //     const testCase07 = new TestCase07();
-    //     testCase07.testCase07();
-    // });
-    // it('Test Case No.8', () => {
-    //     const testCase08 = new TestCase08();
-    //     testCase08.testCase08();
-    // });
-    // it('Test Case No.9', () => {
-    //     const testCase09 = new TestCase09();
-    //     testCase09.testCase09();
-    // });
+    it('First Test Case', () => {
+        const firstTestCase = new TestCase01();
+        firstTestCase.TestCase01();
+    });
+    it('Test Case No.2', () => {
+        const testCase02 = new TestCase02();
+        testCase02.testCase02();
+    });
+    it('Test Case No.3', () => {
+        const testCase03 = new TestCase03();
+        testCase03.testCase03();
+    });
+    it('Test Case No.4', () => {
+        const testCase04 = new TestCase04();
+        testCase04.testCase04();
+    });
+    it('Test Case No.5', () => {
+        const testCase05 = new TestCase05();
+        testCase05.testCase05();
+    });
+    it('Test Case No.6', () => {
+        const testCase06 = new TestCase06();
+        testCase06.testCase06();
+    });
+    it('Test Case No.7', () => {
+        const testCase07 = new TestCase07();
+        testCase07.testCase07();
+    });
+    it('Test Case No.8', () => {
+        const testCase08 = new TestCase08();
+        testCase08.testCase08();
+    });
+    it('Test Case No.9', () => {
+        const testCase09 = new TestCase09();
+        testCase09.testCase09();
+    });
     it('Test Case No.10', () => {
         const testCase10 = new TestCase10();
         testCase10.testCase10();
